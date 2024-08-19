c93933f22ae616b826573db98f400dc1e66d33d6
dziubat <125741590+dziubat@users.noreply.github.com>
2024-08-19 07:42:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -50,3 +50,15 @@ function mostraResultado(afirmacao) {
 
 // Inicializa a primeira pergunta
 mostraPergunta();
+
+function aleatorio (lista){
+    const posicao = Math.floor(Math.random()*lista.length);
+    return lista[posicao];
+}
+
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
+    historiaFinal += afirmacoes + '';
+    atual++;
+    mostraPergunta();
+    }
